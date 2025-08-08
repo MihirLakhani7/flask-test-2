2a8b300aee241d9d65ca087f18e4e0d2ff5ae06b
MihirLakhani7 <mihirlakhani7458@gmail.com>
2025-08-08 08:26:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -180,4 +180,5 @@ def contact():
     return render_template('index.html', success=True)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+
+    app.run(host="0.0.0.0", debug=True, port=5000)
